2eec1c0841b12f15c7da8f6ba5b7905472914878
Tomer Aharoni <t.aharoni@columbia.edu>
2018-01-09 15:53:52 +0100

    Modified app.py to add ta2453 as a subdirectory

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template('index.html')
+    return render_template('/index.html')
 
+@app.route('/ta2453', methods=['GET'])
+def index():
+    return render_template('/ta2453.html')
 
 if __name__ == '__main__':
     app.run()
